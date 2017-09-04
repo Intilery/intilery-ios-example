195e4ca468ab1799d726cc2c89ac5eb504a4f970
Tom McMillen <tom.mcmillen@intilery.com>
2017-09-04 21:24:49 +0100

    Initial Commit

---
+++ HelloIntilery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloIntilery
+//
+//  Created by Tom McMillen on 04/09/2017.
+//  Copyright © 2017 Intilery.com Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloIntilery/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloIntilery
+//
+//  Created by Tom McMillen on 04/09/2017.
+//  Copyright © 2017 Intilery.com Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
